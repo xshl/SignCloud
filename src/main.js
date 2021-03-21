abc6391489a7c9e6c8066959af68aef56d58a83c
xshl <s839806383@163.com>
2021-03-21 13:43:40 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "@/router";
+import store from "@/store";
+import VueFluent from "vfluentdesign";
+import "vfluentdesign/lib/index.css";
+
+Vue.use(VueFluent);
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/home/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+      <fv-button icon="CalculatorAddition" theme="dark">Add</fv-button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
