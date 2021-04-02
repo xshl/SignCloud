e7495dc81145b571e224269a6d116155fd95cf28
xshl <s839806383@163.com>
2021-04-02 11:14:49 +0800

    fix: update http interceptors

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,18 +3,18 @@ import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
-// create an axios instance
+//创建axios实例
 const service = axios.create({
   baseURL: "http://59.77.134.88:8080/", // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
 
-// request interceptor
+// request拦截器
 service.interceptors.request.use(
   config => {
     // do something before request is sent
-
+    config.headers['Content-Type'] = 'application/json'
     if (store.getters.token) {
       // let each request carry token
       // ['X-Token'] is a custom headers key
@@ -30,7 +30,7 @@ service.interceptors.request.use(
   }
 )
 
-// response interceptor
+// response拦截器
 service.interceptors.response.use(
   /**
    * If you want to get http information such as headers or status
